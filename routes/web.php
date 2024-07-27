f2d2e1c0ef61b41866b0a9fecd81b410c025663c
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-27 18:44:18 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::redirect('/','/dashboard');
 
 // Route::get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
