7772e0251f244abe52da95317a7ebd4a35140346
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-29 16:03:45 +0300

    lets call it fly.it

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -86,6 +86,12 @@ export default function Login({ status, canResetPassword }) {
                             Forgot your password?
                         </Link>
                     )}
+                    <Link
+                            href={route('register')}
+                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 p-3 border border-black mx-4"
+                        >
+                            Register
+                        </Link>
 
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
